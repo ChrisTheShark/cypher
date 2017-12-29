e9b3398ee59b3b144662589072411d809929b7d3
localadmin <localadmin@mn-c02v624qhtd8.allstate.com>
2017-12-29 14:52:23 -0500

    Updating cypher to allow overlap.

--- caesar.go
+++ caesar.go
@@ -5,6 +5,24 @@ import (
 	"bytes"
 )
 
+const (
+	maxLetterInt = 122
+	minLetterInt = 96
+)
+
+var lettersToInt = make(map[string]int)
+var intToLetters = make(map[int]string)
+
+func init() {
+	letterString := "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+	lettersSplit := strings.Split(letterString, ",")
+
+	for idx, letter := range lettersSplit {
+		lettersToInt[letter] = idx + 97
+		intToLetters[idx + 97] = letter
+	}
+}
+
 // Consume a message string, convert to lowercase and then to a slice of
 // runes. From here we can use basic operators to shift the characters the
 // desired amount.
@@ -13,8 +31,15 @@ func Shift(message string, shift int) (string, error) {
 	convertedSlice := []rune{}
 
 	for _, char := range originalSlice {
-		newChar := char + rune(shift % 26)
-		convertedSlice = append(convertedSlice, newChar)
+		straightShift := int(char) + shift
+		if letter, ok := intToLetters[straightShift]; ok {
+			newChar := rune(lettersToInt[letter])
+			convertedSlice = append(convertedSlice, newChar)
+		} else {
+			letter := intToLetters[(straightShift - maxLetterInt) + minLetterInt]
+			newChar := rune(lettersToInt[letter])
+			convertedSlice = append(convertedSlice, newChar)
+		}
 	}
 
 	return string(convertedSlice), nil
--- caesar_test.go
+++ caesar_test.go
@@ -9,16 +9,24 @@ import (
 // character value by a desired amount. Receiver would need to know the shift
 // to decode properly.
 func TestShift(t *testing.T) {
-	message := "This is the encoded message"
-	counts, err := CountRunes(message)
-	for key, value := range counts {
-		t.Log(key, value)
-	}
+	message := "thisistheencodedmessage"
 
-	message, err = Shift(message,1)
+	message, err := Shift(message, 1)
 	if err != nil {
 		t.Errorf("Shift invocation failed due to: %v", err.Error())
-	} else if message != "uijt!jt!uif!fodpefe!nfttbhf" {
+	} else if message != "uijtjtuiffodpefenfttbhf" {
+		t.Errorf("Shift invocation resulted in unexpected output.")
+	}
+	t.Logf("Encoded message: %v", message)
+}
+
+func TestShiftOverlap(t *testing.T) {
+	message := "tuvwxyzabcdefghijkl"
+
+	message, err := Shift(message, 1)
+	if err != nil {
+		t.Errorf("Shift invocation failed due to: %v", err.Error())
+	} else if message != "uvwxyzabcdefghijklm" {
 		t.Errorf("Shift invocation resulted in unexpected output.")
 	}
 	t.Logf("Encoded message: %v", message)
